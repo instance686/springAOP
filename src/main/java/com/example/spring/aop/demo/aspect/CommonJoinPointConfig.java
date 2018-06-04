fccf09e01888f6fe231121132e56c3297a0bcf5c
Ayush Srivastava <ayush.cir@gmail.com>
2018-06-05 03:01:41 +0530

    other pointcut annotaions

--- src/main/java/com/example/spring/aop/demo/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/example/spring/aop/demo/aspect/CommonJoinPointConfig.java
@@ -4,6 +4,22 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class CommonJoinPointConfig {
 
-    @Pointcut("execution(* com.example.spring.aop.demo.bussiness.*.*(..))")
+    @Pointcut("execution(* com.example.spring.aop.demo.dao.*.*(..))")
     public void datalayerExecution(){}
+
+    @Pointcut("execution(* com.example.spring.aop.demo.bussiness.*.*(..))")
+    public void bussinesslayerExecution(){}
+
+
+    @Pointcut("com.example.spring.aop.demo.aspect.CommonJoinPointConfig.datalayerExecution() && com.example.spring.aop.demo.aspect.CommonJoinPointConfig.bussinesslayerExecution()")
+    public void allLayerExecution(){}
+
+    @Pointcut("bean(Dao*)")
+    public void beanStartingWithDao(){}
+
+    @Pointcut("bean(*dao*)")
+    public void beanContaining(){}
+
+    @Pointcut("within(com.example.spring.aop.demo.dao..*)")
+    public void datalayerExecutionWithWithin(){}
 }
