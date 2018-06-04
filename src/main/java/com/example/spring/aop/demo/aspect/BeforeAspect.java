9f1233fd3e202cc8f9845ba66818ba2b696b0ea0
Ayush Srivastava <ayush.cir@gmail.com>
2018-06-05 02:14:55 +0530

    AOP Joinpoint

--- src/main/java/com/example/spring/aop/demo/aspect/BeforeAspect.java
+++ src/main/java/com/example/spring/aop/demo/aspect/BeforeAspect.java
@@ -1,4 +1,25 @@
 package com.example.spring.aop.demo.aspect;
 
+import com.example.spring.aop.demo.DemoApplication;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
 public class BeforeAspect {
+    private Logger logger=LoggerFactory.getLogger(BeforeAspect.class);
+
+
+    //execution (* PACKAGE.*.*(..))
+    //           |         | |
+    // any returntype  class method
+    @Before("execution(* com.example.spring.aop.demo.bussiness.*.*(..))")
+    public void before(JoinPoint joinPoint){
+        logger.info("Intercepted method calls {}",joinPoint);
+    }
+
 }
